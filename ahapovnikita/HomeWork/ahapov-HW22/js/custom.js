7454490a4d3b5d166948732e10c91660a4b0be3f
ahapov <agapowtutaj@gmail.com>
2022-01-30 20:33:08 +0200

    hw23

--- ahapovnikita/HomeWork/ahapov-HW22/js/custom.js
+++ ahapovnikita/HomeWork/ahapov-HW22/js/custom.js
@@ -1,13 +1,67 @@
 jQuery.noConflict();
 jQuery(document).ready(function () {
-    'use strict';
+    'use strict'
 
     jQuery(".fancybox").fancybox();
+
+    let carousel = jQuery('#carousel-partners');
+
+    carousel.owlCarousel({
+        items: 4,
+        autoplay: 1000,
+        autoplayTimeout: 6000,
+        autoplayHoverPause: false,
+        smartSpeed: 1000,
+        dotsSpeed: 1000,
+        loop: true,
+        nav: false,
+        navText: [""],
+        dots: true,
+        mouseDrag: true,
+        margin: 30,
+        stagePadding: 0,
+        autoWidth: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+                nav: false,
+                mouseDrag: true,
+                autoplay: true,
+                smartSpeed: 1000,
+            },
+            479: {
+                items: 1,
+                dots: true,
+                mouseDrag: true,
+                autoplay: true,
+                smartSpeed: 1000,
+            },
+            767: {
+                items: 2,
+                dots: true,
+                mouseDrag: true,
+                autoplay: true,
+                smartSpeed: 1000,
+            },
+            991: {
+                items: 3,
+                dots: true,
+                mouseDrag: true,
+                autoplay: true,
+                smartSpeed: 1000,
+            },
+            1099: {
+                items: 4,
+                dots: true,
+                mouseDrag: true,
+            },
+        },
+    });
 });
 
-"use strict";
-
-
+("use strict");
 
 function r(f) {
     /in/.test(document.readyState) ? setTimeout("r(" + f + ")", 9) : f();
