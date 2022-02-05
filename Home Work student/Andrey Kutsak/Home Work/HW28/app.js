92dc22c4ea29e5119e6d47b2a651fd641abe307f
AndreyKutsak <andreykutsak.aak@gmail.com>
2022-02-05 23:59:58 +0200

    HW28

--- Home Work student/Andrey Kutsak/Home Work/HW28/app.js
+++ Home Work student/Andrey Kutsak/Home Work/HW28/app.js
@@ -1,25 +1,47 @@
-const people = [
-  { name: "Игорь", surname: "surname1", age: 14, salary: 400000 },
-  { name: "Марина", surname: "surname2", age: 30, salary: 800000 },
-  { name: "Андрей", surname: "surname3", age: 44, salary: 3400 },
-  { name: "Олег", surname: "surname4", age: 16, salary: 1000 },
-  { name: "Даша", surname: "surname5", age: 15, salary: 2300 },
-  { name: "Таня", surname: "surname6", age: 28, salary: 25000 },
-  { name: "Іван", surname: "surname7", age: 22, salary: 25000 },
-];
+document.addEventListener("DOMContentLoaded", function () {
+  document.body.style.margin = "0px";
+  let canvas = document.getElementById("canvas");
+  let ctx = canvas.getContext("2d");
+  let workers = [
+    { name: "Andrey", surname: "Kutsak", salary: 83000 },
+    { name: "Ivan", surname: "Andreyev", salary: 73000 },
+    { name: "Marina", surname: "Mahalyas", salary: 63000 },
+    { name: "Yana", surname: "Smychok", salary: 53000 },
+    { name: "Viktor", surname: "Ivanenko", salary: 53000 },
+    { name: "Alla", surname: "kolosov", salary: 53000 },
+    { name: "Ruslan", surname: "Ivanov", salary: 400 },
+    { name: "Vitaliy", surname: "Onyshcuk", salary: 5000 },
+    { name: "Roman", surname: "Re", salary: 63000 },
+    { name: "Lora", surname: "Virginia", salary: 63000 },
+  ];
+  let lineHeight = 30;
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
 
-let users = people.filter(function (a) {
-  if (a.name === "Олег" || a.name === "Таня") {
-    return a;
+  ctx.fillStyle = "#000";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "red";
+  ctx.fillRect(20, 30, 100, 100);
+
+  ctx.fillStyle = "yellow";
+  ctx.beginPath();
+  ctx.arc(250, 150, 50, 0, 2 * Math.PI);
+  ctx.fill();
+
+  ctx.font = "25px sans-serif";
+  ctx.fillStyle = "#fff";
+  ctx.fillText("list people who has salary more then 5000 dolars", 40, 270);
+  for (const i of workers) {
+    if (i.salary > 5000) {
+      ctx.fillStyle = "green";
+      ctx.font = "20px sans-serif";
+      ctx.fillText(
+        i.name + " " + i.surname + " have salary " + i.salary,
+        40,
+        lineHeight + 300
+      );
+      lineHeight += 30;
+    }
   }
 });
-console.log(users);
-
-let sum = people.reduce(function (a, b) {
-  return a + b.salary;
-}, 0);
-console.log(sum);
-let usersData = people.map(function (a, b) {
-  return "name " + a.name + " surname " + a.surname;
-});
-console.log(usersData);
