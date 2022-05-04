74c9e4b319508271c7e328e37f6f893f9a087eca
Andrey Kutsak <andriykutsak.ak@gmail.com>
2022-05-04 09:31:03 +0300

    remove old home works

--- Home Work student/Andrey Kutsak/Home Work/HW23/script.js
+++
@@ -1,7 +0,0 @@
-let num = "123";
-num2 = "24";
-console.log(Number(num) + +num2);
-let a = 10;
-console.log(a);
-a = 20;
-console.log(a);
--- Home Work student/Andrey Kutsak/Home Work/HW24/script.js
+++
@@ -1,6 +0,0 @@
-let a = 10;
-let b = 2;
-let c = 5;
-let d = a + b + c;
-let el = document.getElementById("sum");
-el.innerText = d;
