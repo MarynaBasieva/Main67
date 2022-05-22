5e71e1fb6270dc07fea1e7f86348e6a1dbfd9ef1
Andrey Kutsak <andriykutsak.ak@gmail.com>
2022-05-22 15:30:38 +0300

    make project 5

--- Home Work student/Andrey Kutsak/Home Work/HW37/app.js
+++ Home Work student/Andrey Kutsak/Home Work/HW37/app.js
@@ -1,1 +1,28 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+	$("#dropdown-item").click(() => {
+		$(".dropdown-menu").toggle("slow");
+	});
+	$("#show-menu").click(() => {
+		$(".form-wraper").toggleClass("active");
+	});
+	$("#popup-btn,.close-btn,#popupBg").on("click", (event) => {
+		event.stopPropagation();
+		let t = event.target.id;
+		if (t == "popupBg" || t == "popup-btn" || t == "close") {
+			toogleModal(".popup-wraper");
+		}
+	});
+	let toogleModal = (a) => {
+		let h = Number($(String(a)).height()),
+			chekErr = isNaN(h);
+		if (!chekErr) {
+			console.log(h);
+			if (h === 0) {
+				console.log(a);
+				$(String(a)).css({ height: "100%", width: "100%" });
+			} else {
+				$(String(a)).css({ height: "0%", width: "0%" });
+			}
+		}
+	};
+});
